20cc50d2d15a0a7b83c004052b04360b1f9c2260
xander naumenko <xandernaumenko@gmail.com>
2019-11-01 18:26:14 -0700

    Started testing streaming functionality

--- LabjackLibraryTesting/LabjackLibraryTesting.ino
+++ LabjackLibraryTesting/LabjackLibraryTesting.ino
@@ -9,12 +9,18 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 178);
 byte server[] = { 192, 168, 1, 214 }; // Labjack
 
-float reg = -1;
-float value;
+float STREAM_SCANRATE_HZ = 5;
+float STREAM_NUM_ADDRESSES = 2;
+float STREAM_SAMPLES_PER_PACKET = 5;
+float STREAM_AUTO_TARGET = 0x01;
+float address1 = 0;
+float address2 = 2;
 
 EthernetClient client;
 ModbusTCPClient modbusTCPClient(client);
-
+EthernetClient stream;
+ModbusTCPClient modbusStream(stream);
+ 
 void setup() { 
   // Required because the WIZ820io requires a reset pulse and the Teensy doesn't have a reset pin (pin 9 is used instead)
   pinMode(9, OUTPUT);
@@ -30,12 +36,30 @@ void setup() {
   Ethernet.begin(mac, ip);
   Serial.begin(9600);
   LJM_Open(&client, &modbusTCPClient, server);
+  if (!modbusStream.begin(server, 702)) {
+      Serial.println("Modbus TCP Client failed to connect!");
+  } else {
+      Serial.println("Modbus connected");
+  } 
+  LJM_eWriteAddress(&modbusTCPClient, 4002, 3, STREAM_SCANRATE_HZ);
+  LJM_eWriteAddress(&modbusTCPClient, 4004, 1, STREAM_NUM_ADDRESSES);
+  LJM_eWriteAddress(&modbusTCPClient, 4006, 1, STREAM_SAMPLES_PER_PACKET);
+  LJM_eWriteAddress(&modbusTCPClient, 4016, 1, STREAM_AUTO_TARGET);
+  LJM_eWriteAddress(&modbusTCPClient, 4100, 1, address1);
+  LJM_eWriteAddress(&modbusTCPClient, 4102, 1, address2);
+  LJM_eWriteAddress(&modbusTCPClient, 4990, 1, (float)1);
+  delay(3000);
+  LJM_eWriteAddress(&modbusTCPClient, 4990, 1, (float)0);
 }
 
 void loop() {
-  LJM_eWriteAddress(&modbusTCPClient, 55120, 1, reg);
-  LJM_eReadAddress(&modbusTCPClient, 55120, 1, &value);
+  
+//  LJM_eReadAddress(&modbusTCPClient, 4006, 0, &value);
 //  Serial.println(value, HEX);
-  Serial.println(value);
-  delay(2000);
+//  Serial.println(value);
+  while(stream.available()){
+    Serial.println(stream.read());
+  }
+  Serial.println("test");
+  delay(100);
 }
